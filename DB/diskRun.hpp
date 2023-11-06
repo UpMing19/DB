910e6b3e1dfefd66f3dc60fcec459f1d6ffd774c
MingYu Wang <63656745+UpMing19@users.noreply.github.com>
2023-11-06 14:48:08 +0000

    disk

--- DB/diskRun.hpp
+++ DB/diskRun.hpp
@@ -70,7 +70,7 @@ public:
         }
 
         result = write(fd, "", 1);
-        if (result !=1)
+        if (result != 1)
         {
             close(fd);
             perror("Error writing last byte of the file ");
@@ -95,35 +95,167 @@ public:
             exit(EXIT_FAILURE);
         }
     }
-    void setCapacity(unsigned long newCap){
+    void setCapacity(unsigned long newCap)
+    {
         _capacity = newCap;
     }
-    unsigned long getCapacity(){
+    unsigned long getCapacity()
+    {
         return _capacity;
     }
-    void writeData(const KVPair_t *run,const size_t offset,const unsigned long len ){
-        memcpy(map+offset,run,len * sizeof(KVPair_t));
+    void writeData(const KVPair_t *run, const size_t offset, const unsigned long len)
+    {
+        memcpy(map + offset, run, len * sizeof(KVPair_t));
         _capacity = len;
     }
-    void constructIndex(){
-        _fencePointers.reserve(_capacity/pageSize);
+    void constructIndex()
+    {
+        _fencePointers.reserve(_capacity / pageSize);
         _iMaxFP = -1;
-        for(int j =0 ; j<_capacity;j++){
-            bf.add((K*) &map[j].key,sizeof(K));
-            if(j%pageSize==0){
+        for (int j = 0; j < _capacity; j++)
+        {
+            bf.add((K *)&map[j].key, sizeof(K));
+            if (j % pageSize == 0)
+            {
                 _fencePointers.push_back(map[j].key);
                 _iMaxFP++;
             }
         }
 
-        if(_iMaxFP >=0 )
-            _fencePointers.resize(_iMaxFP+1);
-        
-        minKey = map[0].key;
-        maxKey = map[_capacity-1].key;
+        if (_iMaxFP >= 0)
+            _fencePointers.resize(_iMaxFP + 1);
 
+        minKey = map[0].key;
+        maxKey = map[_capacity - 1].key;
     }
-    
+    unsigned long binary_search(const unsigned long offset, const unsigned long n, const K &key, bool &found)
+    {
+        unsigned long L = offset, R = offset + n - 1;
+        if (n == 0)
+        {
+            found = false;
+            return offset;
+        }
+        while (L <= R)
+        {
+            unsigned mid = (L + R) >> 1;
+            if (map[mid].key < key)
+            {
+                L = mid + 1;
+            }
+            else if (map[mid].key > key)
+            {
+                R = mid - 1;
+            }
+            else
+            {
+                found = true;
+                return mid;
+            }
+        }
+        found = false;
+        return L;
+    }
+
+    void get_flanking_range(const K &key, const unsigned long &start, const unsigned long &end)
+    {
+        if (_iMaxFP <= 0)
+        {
+            start = 0;
+            end = _capacity;
+        }
+        else if (key < _fencePointers[1])
+        {
+            start = 0;
+            end = _capacity;
+        }
+
+        else if (key >= _fencePointers[_iMaxFP])
+        {
+            start = pageSize * _iMaxFP;
+            end = _capacity;
+        }
+        else
+        {
+            unsigned L = 0, R = _iMaxFP;
+            while (L <= R)
+            {
+                unsigned mid = (L + R) >> 1;
+                if (_fencePointers[mid] < key)
+                {
+
+                    if (_fencePointers[mid + 1] >= key)
+                    {
+                        start = pageSize * mid;
+                        end = pageSize * (mid + 1);
+                        return;
+                    }
+
+                    L = mid + 1;
+                }
+                else if (_fencePointers[mid] > key)
+                {
+                    if (_fencePointers[mid - 1] <= key)
+                    {
+                        start = pageSize * (mid - 1);
+                        end = pageSize * mid;
+                        return;
+                    }
+                    R = mid - 1;
+                }
+                else
+                {
+                    start = mid * pageSize;
+                    end = start;
+                }
+            }
+        }
+    }
+
+    void get_index(const K &key, bool &found)
+    {
+        unsigned long start, end;
+        get_flanking_range(key, start, end);
+        return binary_search(start, end - start + 1, key, found);
+    }
+
+    V lookup(const K &key, bool &found)
+    {
+        unsigned long idx = get_index(key, found);
+        return found ? map[idx].value : V(NULL);
+    }
+
+    void range(const K &key1, const K &key2, unsigned long &i1, unsigned long &i2)
+    {
+        i1 = 0;
+        i2 = 0;
+        if (key2 < minKey || key1 > maxKey)
+            return;
+        if (key1 >= minKey)
+        {
+            bool found = false;
+            i1 = get_index(key1, found);
+        }
+        if (key2 > maxKey)
+        {
+            i2 = _capacity;
+            return;
+        }
+        else
+        {
+            bool found = false;
+            i2 = get_index(key2, found);
+        }
+    }
+    void printElts()
+    {
+        for (int j = 0; j < _capacity; j++)
+        {
+            cout << map[j].key << " ";
+        }
+        cout << endl;
+    }
+
 private:
     unsigned long _capacity;
     string _filename;
@@ -132,4 +264,23 @@ private:
     unsigned _iMaxFP;
     unsigned _runID;
     double _bf_fp;
+
+    void doMap()
+    {
+        size_t filesize = _capacity * sizeof(KVPair_t);
+
+        fd = open(_filename.c_str(), O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+        if (fd == -1)
+        {
+            perror("Error opening file for writing");
+            exit(EXIT_FAILURE);
+        }
+        map = (KVPair<K, V> *)mmap(0, filesize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        if (map == MAP_FAILED)
+        {
+            close(fd);
+            perror("Error mmapping the file");
+            exit(EXIT_FAILURE);
+        }
+    }
 };
